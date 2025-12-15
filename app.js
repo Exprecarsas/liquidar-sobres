929b587768b1a4589d12939832f3bfccec20ddb8
Exprecar sas <sistemas@exprecar.com>
2025-12-15 09:07:31 -0500

    ok

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // ===== CONFIG =====
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxviDpRACUW8GMp3HfsIznOJCbXLcCIXI3qvjL-PcYZLYnIRbRAwcpMpLq1JSPfJfJ_dQ/exec"; // <-- tu /exec
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyX5vn0MvLGpX7xIbXshDmgnb0nPQSDmiHhFVNI1PPRHHNgnGfVtj0sooRsXXSlnGzc/exec"; // <-- tu /exec
   const TIPO_FIJO = "CARGUE";        // SIEMPRE CARGUE
   const AUTOSYNC_MS = 30_000;        // re-carga Liquidaciones cada 30s
   // ==================
